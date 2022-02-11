c2e8e443ebe84bc93aebd9599168641053e6f659
Nathan Laan <nathanlaan@gmail.com>
2022-02-11 11:23:28 -0700

    DEV: Added tests event listeners.

--- background.js
+++ background.js
@@ -1,5 +1,26 @@
+console.log('background');
 // let setting = '';
 
 chrome.runtime.onInstalled.addListener(() => {
-  //chrome.storage.sync.set({ setting });
+  console.log('onInstalled');
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  console.log('onStartup');
+});
+
+chrome.bookmarks.onChanged.addListener((id, info) => {
+  console.log('bookmarks.onChanged: ' + JSON.stringify(info));
+});
+
+chrome.bookmarks.onCreated.addListener((id, bookmark) => {
+  console.log('bookmarks.onCreated: ' + JSON.stringify(bookmark));
+});
+
+chrome.bookmarks.onRemoved.addListener((id, info) => {
+  console.log('bookmarks.onCreated: ' + JSON.stringify(info));
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  console.log(`Command: ${command}`);
 });
