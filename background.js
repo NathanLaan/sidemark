720affceb34d3c838f332aef75918b755337b6eb
Nathan Laan <nathanlaan@gmail.com>
2022-02-11 23:03:20 -0700

    DEV: Testing message-passing functions.

--- background.js
+++ background.js
@@ -3,6 +3,29 @@ console.log('background');
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled');
+
+  //
+  // TODO: Save default settings.
+  //
+
+  //
+  // Testing message-passing functions
+  //
+
+  /*
+  chrome.runtime.sendMessage({ message: 'sendMessage:onInstalled' },
+    function (response) {
+      console.log(JSON.stringify(response));
+    });
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { message: 'chrome.tabs.query:onInstalled' },
+      function (response) {
+        console.log(response);
+      });
+  });
+  */
+
 });
 
 chrome.runtime.onStartup.addListener(() => {
