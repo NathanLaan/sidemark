f9aa954ac1cb537a9638e3923bb07b1c667acbc1
Nathan Laan <nathanlaan@gmail.com>
2022-02-18 23:34:13 -0700

    fix: Add content CSS file. Fix floating div issue.

--- content.js
+++ content.js
@@ -3,16 +3,8 @@ msg('content');
 let visible = false;
 
 let element = document.createElement('div'); 
-element.style.background = "orange";
-element.style.clear = "both";
-element.style.display = "float";
-element.style.height = "100%";
-element.style.width = "300px";
-element.style.position = "fixed";
-element.style.top = "0px";
-element.style.right = "0px";
-element.style.zIndex = "9000000000000000000";
-element.frameBorder = "none"; 
+element.className = 'sidemark-sidebar';
+element.frameBorder = "none";
 
 function msg(m) {
   chrome.runtime.sendMessage({message: m});
