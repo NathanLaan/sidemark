a317a755cea495e71c35129170bae6515ee40cd7
Nathan Laan <nathanlaan@gmail.com>
2022-02-19 00:20:35 -0700

    fix: Only showing on chrome.action.onClicked

--- content.js
+++ content.js
@@ -2,22 +2,21 @@ msg('content');
 
 let visible = false;
 
-let element = document.createElement('div'); 
+let element = document.createElement('div');
 element.className = 'sidemark-sidebar';
 
 function msg(m) {
-  chrome.runtime.sendMessage({message: m});
+  chrome.runtime.sendMessage({ message: m });
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-  if(visible) {
-    document.body.removeChild(element);
-  } else{
-    document.body.appendChild(element);
+chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+  if(message === 'chrome.action.onClicked') {
+    if (visible) {
+      document.body.removeChild(element);
+    } else {
+      document.body.appendChild(element);
+    }
+    visible = !visible;
   }
-  visible = !visible;
 });
 
-
-
- 
